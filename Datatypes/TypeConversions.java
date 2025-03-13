e9bbb7f75b94f27c20a8786be95bf16cbcbb9982
Venkat1506ak <139452463+Venkat1506ak@users.noreply.github.com>
2025-03-13 14:09:26 +0530

    Update TypeConversions.java

--- Datatypes/TypeConversions.java
+++ Datatypes/TypeConversions.java
@@ -3,8 +3,10 @@ class TypeConversions{
 		float a=2.12f;
 		double b=3.123;
 		int c=23;
+		double d=3;
 		System.out.println((int)a);
 		System.out.println((float)b);
 		System.out.println((short)c);
+		System.out.println(d);
 	}
 }
